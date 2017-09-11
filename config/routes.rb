04c44dbe1d31f52dde7369ffe561596922d480f4
Vlados11 <vladislavva@123software.ru>
2017-09-11 20:10:31 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resource :projects do
-        resource :tasks
+      resources :projects, except: :show do
+        resources :tasks, except: :show
       end
     end
   end
